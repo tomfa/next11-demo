148553a028016a449d2b03c0027af08c484c479c
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-08-18 21:21:41 +0200

    style: add scss support

--- pages/styles/index.tsx
+++ pages/styles/index.tsx
@@ -1,18 +1,30 @@
-import type { NextPage } from 'next'
-import styles from '../../components/Button.module.css'
-import otherStyles from '../../components/OtherButton.module.css'
+import type { NextPage } from "next";
+import styles from "../../components/Button.module.scss";
+import otherStyles from "../../components/OtherButton.module.css";
 
 const Index: NextPage = () => {
   return (
-    <section style={{ display: 'flex', flexDirection: 'column', maxWidth: '250px', padding: '20px'}}>
+    <section
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        maxWidth: "250px",
+        padding: "20px",
+      }}
+    >
       <h2>Styles</h2>
       <button>Regular button</button>
-      <button className={'error'}>Global error styling</button>
+      <button className={"error"}>Global error styling</button>
       <button className={styles.error}>Moduled error button</button>
-      <button className={otherStyles.error}>.error class from other module</button>
-      <button style={{ backgroundColor: 'hotpink' }}>Inline styling</button>
+      <button className={styles.error}>
+        <span className={styles.highlightedText}>Highlighted error button</span>
+      </button>
+      <button className={otherStyles.error}>
+        .error class from other module
+      </button>
+      <button style={{ backgroundColor: "hotpink" }}>Inline styling</button>
     </section>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
