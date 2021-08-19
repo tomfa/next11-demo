9e9e5fc31c8e5de49b92d8e1f5c0ca588dd93464
Tomas Fagerbekk <tomas.a.fagerbekk@gmail.com>
2021-08-19 09:01:27 +0200

    auth: add auth0 provider login

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+/* eslint-disable no-param-reassign, unused-imports/no-unused-vars */
+import NextAuth, { Account, Profile, Session, User } from 'next-auth';
+import Providers from 'next-auth/providers';
+
+export default NextAuth({
+  providers: [
+    Providers.Auth0({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      domain: process.env.AUTH0_DOMAIN,
+    })
+  ],
+  secret: process.env.JWT_SECRET,
+});
